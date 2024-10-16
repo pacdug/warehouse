b22290302189229d89725a9b4662d69225b94156
pacdug <buitiendung091003@gmail.com>
2024-10-16 09:57:52 +0700

    first commit

---
+++ mang/taovathaotac/react.js
@@ -0,0 +1,16 @@
+let x='';
+let array= Array();
+function add_Element_to_array() {
+    array[x] =document.getElementById('arrvl').value ;
+    alert("Element: " + array[x] + " Added at index " + x);
+    x++;
+    document.getElementById('arrvl').value = '';
+}
+
+function Display_array() {
+    let e = "<hr/>";
+    for (let i = 0; i < array.length; i++) {
+        e += "Element " + i + " = " + array[i] + "<br/>";
+    }
+    document.getElementById("result").innerHTML = e;
+}
