2b8f88dad2694fb6e61e897dcd5b9358eb2c600b
pacdug <buitiendung091003@gmail.com>
2024-10-17 14:11:29 +0700

    first commit

--- mang1/max/react.js
+++ mang1/max/react.js
@@ -1,9 +1,9 @@
 let numbers = [-3, 5, 1, 3, 2, 10];
-let max = numbers[0];
+let min = numbers[0];
 for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-        max = numbers[i];
+    if (numbers[i] < min) {
+        min = numbers[i];
         index = i ;
     }
 }
-alert("max: " + max + " at position " + index);
+document.write("min: " + min + " at position " + index);
---
+++ mang2/sudunghamcosan/bai3/react.js
@@ -0,0 +1,17 @@
+function swapCase(str){
+    let result = '';
+    for(let i=0;i<str.length;i++){
+        let char = str[i];
+        if(char === char.toLowerCase()){
+            result +=char.toUpperCase();
+        }else {
+            result +=char.toLowerCase();
+        }
+    }
+    return result;
+}
+
+let inputString = prompt("Nhập vào một chuỗi:");
+let swappedString = swapCase(inputString);
+
+document.write(`chuoi truoc khi chuyen doi ${swappedString}`);
